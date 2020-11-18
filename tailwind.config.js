97a8d10e24da13719fa501c11999e881dc726296
ldantas-s <lucianostudant@gmail.com>
2020-11-18 18:12:17 -0300

    Configure the script, build: css-prod
    and build: css-dev to generate tailwindcss optimized css files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+		'./src/**/*.jsx',
+		'./src/**/*.js',
+	],
   theme: {
     extend: {},
   },
