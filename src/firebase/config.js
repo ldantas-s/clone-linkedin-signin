914ca2c49cfb666b0cf9976b8040bdb0a853d2c0
ldantas-s <lucianostudant@gmail.com>
2021-02-01 19:54:29 -0300

    Feat: add the environments variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,14 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCG3O0MV3GCBxbcCY_3sN57QgHDntkIpo4",
-  authDomain: "authbootstrap.firebaseapp.com",
-  databaseURL: "https://authbootstrap.firebaseio.com",
-  projectId: "authbootstrap",
-  storageBucket: "authbootstrap.appspot.com",
-  messagingSenderId: "285794479",
-  appId: "1:285794479:web:8bd4cc0c5bf9f3ee09345a"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
