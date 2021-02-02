0eb58950ed8207b3ffa0c26a399886c74ab72a57
ldantas-s <lucianostudant@gmail.com>
2021-02-02 20:44:02 -0300

    Feat: add PrivateRoute router
    add context ProvideAuth

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
+// Pages
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Home from './pages/Home.jsx';
 
+// Context
+import ProvideAuth, { useAuth } from './context/authContext.js';
+
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/home" component={Home} />
-    </BrowserRouter>
+		<ProvideAuth>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={SignIn} />
+					<Route path="/signup" component={SignUp} />
+					<PrivateRoute path="/home">
+						<Home />
+					</PrivateRoute>
+				</Switch>
+			</BrowserRouter>
+		</ProvideAuth>
   );
+}
+
+function PrivateRoute({ children, ...rest }) {
+	const auth = useAuth();
+	return (
+		<Route
+			{...rest}
+			render={({ location }) => auth.user ? ( children ) : ( <Redirect to={{ pathname: '/', state: { from: location } }} /> ) }
+		/>
+	)
+
 }
 
 export default Routes;
