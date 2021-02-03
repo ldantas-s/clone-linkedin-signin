8a253f6739e6a7b775de65d734d3abe7bd91f9e6
ldantas-s <lucianostudant@gmail.com>
2021-02-03 20:58:30 -0300

    Feat: add empty object in sessionStorage on signout
    add more information in the page
    remove the use of the async/await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 
 import signOut from '../hooks/signOut';
+import onAuth from '../hooks/onAuth';
 
 import ButtonLinkedin from '../components/ButtonLinkedin.jsx';
 import { useHistory } from 'react-router-dom';
+import { useAuth } from '../context/authContext';
 
 const Home = () => {
 	const history = useHistory();
+	const { user, setUser } = useAuth();
 
-	async function logout() {
-		await signOut()
-			.then((result) => console.log(result))
+	function logout() {
+		signOut()
+			.then((result) => {
+				setUser(onAuth());
+				sessionStorage.setItem('userInfoSession', '{}');
+				history.push('/');
+			})
 			.catch((err) => console.log(err));
-		history.push('/');
 	}
 
 	return (
-		<>
+		<section className="h-screen flex flex-col justify-center w-full px-6 py-12">
+			<h1 className="text-3xl">Welcome to the Linkedin</h1>
+			<h2 className="text-2xl mb-3 font-medium">{user.email}</h2>
 			<ButtonLinkedin onClick={() => logout()}>Sign Out</ButtonLinkedin>
-		</>
+		</section>
 	);
 };
 
