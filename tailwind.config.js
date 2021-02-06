d0331b3c06f6cbd5eceeb9a3ea62a0f1cb9077f0
ldantas-s <lucianostudant@gmail.com>
2021-02-05 22:04:23 -0300

    Feat: add animation to notFound page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,21 @@ module.exports = {
 				goCome: {
 					'0%, 100%': { transform: 'translateX(-1rem)' },
 					'50%': { transform: 'translateX(13rem)' }
+				},
+				goUp: {
+					from: { transform: 'translateY(100px)'},
+					to: { transform: 'translateY(0)'}
+				},
+				goDown: {
+					from: { transform: 'translateY(-45px)'},
+					to: { transform: 'translateY(0)'}
 				}
 			},
 			animation: {
-				'go-come': '2s linear infinite goCome'
+				'go-come': '2s linear infinite goCome',
+				'goup': '.5s linear goUp',
+				'godown': '.5s linear goDown',
+
 			}
 		},
   },
